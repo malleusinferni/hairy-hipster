26ed9bfb4a2ade298950925b2d57f4e812c7666c
Mako <jlauve@rsmw.net>
2013-08-20 19:14:55 -0400

    Change wellness thresholds

--- Main.hs
+++ Main.hs
@@ -3,7 +3,8 @@ data Entity = Player { hp :: Int }
 playTurn = tellHealth . hp
 
 tellHealth hp
-  | hp > 0 = "You feel fine."
+  | hp > 10 = "You feel fine."
+  | hp > 0 = "You feel woozy from blood loss."
   | otherwise = "Your hit points dwindle to zero. You perish!"
 
 main = putStrLn (playTurn (Player 0))
