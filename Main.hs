7d51275246ce18dd3c330fd63b883b082c475ffb
Mako <jlauve@rsmw.net>
2013-08-20 17:58:52 -0400

    Track player HP in state object

--- Main.hs
+++ Main.hs
@@ -1,1 +1,7 @@
-main = putStrLn "Your hit points dwindle to zero. You perish!"
+data Entity = Player { hp :: Int }
+
+playTurn (Player hp)
+  | hp > 0 = "You feel fine."
+  | otherwise = "Your hit points dwindle to zero. You perish!"
+
+main = putStrLn (playTurn (Player 0))
