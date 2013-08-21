d04763a2687e22a195dc8bded2b39069e743d254
Mako <jlauve@rsmw.net>
2013-08-20 21:39:06 -0400

    Model combat more accurately

--- Main.hs
+++ Main.hs
@@ -5,18 +5,24 @@ data Entity = Entity {
   } deriving (Eq, Ord, Show)
 data ID = Player | EID Int deriving (Eq, Ord, Show)
 
-playTurn entities = do
-  weakEntities <- mapM (takeDamage 5) entities
-  let [player] = filter ((== Player) . eid) weakEntities
-      livingEntities = filter ((> 0) . hp) weakEntities
-  putStrLn (tellHealth player)
-  if length livingEntities > 1
-     then playTurn livingEntities
-     else putStrLn $ name (head livingEntities) ++ " wins!"
+playTurn (attacker : defender : bystanders) = do
+  defender <- takeDamage 5 attacker defender
+  let undamaged = bystanders ++ [attacker]
+      survivors
+        | hp defender > 0 = defender : undamaged
+        | otherwise = undamaged
+  if length survivors > 1
+     then playTurn survivors
+     else putStrLn $ name (head survivors) ++ " wins!"
 
-takeDamage amount e = do
-  putStrLn ("Ouch! " ++ name e ++ " takes " ++ show amount ++ " damage.")
-  return (e { hp = hp e - amount })
+takeDamage amount attacker defender = do
+  putStrLn $ unwords ["Ouch!", name attacker, "hits", name defender,
+                      "for", show amount, "damage!"]
+  let def = defender { hp = hp defender - amount }
+  if eid def == Player
+     then putStrLn (tellHealth def)
+     else return ()
+  return def
 
 tellHealth (Entity _ hp _)
   | hp > 10 = "You feel fine."
