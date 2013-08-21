40c78d76f393bfbc7d7cbf330a27efe6281c17f2
Mako <jlauve@rsmw.net>
2013-08-20 20:26:32 -0400

    An entity is not necessarily the player

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
-data Entity = Player { hp :: Int }
+data Entity = Entity { eid :: ID, hp :: Int } deriving (Eq, Ord, Show)
+data ID = Player | EID Int deriving (Eq, Ord, Show)
 
 playTurn = tellHealth . hp
 
@@ -7,4 +8,5 @@ tellHealth hp
   | hp > 0 = "You feel woozy from blood loss."
   | otherwise = "Your hit points dwindle to zero. You perish!"
 
-main = putStrLn (playTurn (Player 0))
+main = putStrLn (playTurn player)
+  where player = Entity { eid = Player, hp = 0 }
