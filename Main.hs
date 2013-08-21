5464726fb43cf20c83840418d19bba37250f6947
Mako <jlauve@rsmw.net>
2013-08-20 20:41:34 -0400

    Handle players with more than 0 HP

--- Main.hs
+++ Main.hs
@@ -1,12 +1,20 @@
 data Entity = Entity { eid :: ID, hp :: Int } deriving (Eq, Ord, Show)
 data ID = Player | EID Int deriving (Eq, Ord, Show)
 
-playTurn = tellHealth . hp
+playTurn player = do
+  weakenedPlayer <- takeDamage player 5
+  putStrLn (tellHealth weakenedPlayer)
+  if hp weakenedPlayer > 0
+     then playTurn weakenedPlayer
+     else return ()
 
-tellHealth hp
+takeDamage e amount = do
+  return (e { hp = hp e - amount })
+
+tellHealth (Entity _ hp)
   | hp > 10 = "You feel fine."
   | hp > 0 = "You feel woozy from blood loss."
   | otherwise = "Your hit points dwindle to zero. You perish!"
 
-main = putStrLn (playTurn player)
+main = playTurn player
   where player = Entity { eid = Player, hp = 0 }
