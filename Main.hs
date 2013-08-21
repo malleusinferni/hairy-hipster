1792708e42c898f5cadd0de0af83d1b0872e9072
Mako <jlauve@rsmw.net>
2013-08-20 22:04:02 -0400

    Random enemy generation

--- Main.hs
+++ Main.hs
@@ -1,11 +1,18 @@
+import System.Random (randomRIO)
+
 data Entity = Entity {
     eid :: ID,
     hp :: Int,
+    species :: Species,
     name :: String
   } deriving (Eq, Ord, Show)
+
+data Species = Shoggoth | Goblin | Merovingian
+  deriving (Eq, Show, Ord, Enum)
+
 data ID = Player | EID Int deriving (Eq, Ord, Show)
 
-playerEntity = Entity Player 25 "player"
+playerEntity = Entity Player 25 Merovingian "player"
 
 playTurn (attacker : defender : bystanders) = do
   defender <- takeDamage 5 attacker defender
@@ -26,11 +33,22 @@ takeDamage amount attacker defender = do
      else return ()
   return def
 
-tellHealth (Entity _ hp _)
+tellHealth (Entity { hp = hp })
   | hp > 10 = "You feel fine."
   | hp > 0 = "You feel woozy from blood loss."
   | otherwise = "Your hit points dwindle to zero. You perish!"
 
-main = playTurn [player, enemy]
-  where player = playerEntity
-        enemy = Entity { eid = EID 0, hp = 44, name = "goblin" }
+randomEnemy = do
+  rid <- randomRIO (0, maxBound)
+  rhp <- randomRIO (5, 35)
+  idx <- randomRIO (fromEnum Shoggoth, fromEnum Merovingian)
+  let spc = toEnum idx :: Species
+      nam = show spc
+  return Entity { eid = EID rid, hp = rhp, name = nam, species = spc }
+
+main = do
+  enemy <- randomEnemy
+  putStrLn "You climb down the well."
+  putStrLn $ unwords ["A", name enemy, "with", show (hp enemy),
+    "HP is lurking below."]
+  playTurn [playerEntity, enemy]
