758d5473662ca4d26e0572507e4c63c10615bed6
Mako <jlauve@rsmw.net>
2013-09-17 17:18:15 -0400

    Include direct objects in more AI actions

--- AI/Action.hs
+++ AI/Action.hs
@@ -1,16 +1,25 @@
-module AI.Action where
+module AI.Action (Action(..)) where
 
 import Support.Coords
 
+type EID = Int
+
 -- Commands which an actor AI may issue in response to Tick
 data Action = Attack -- Damage another entity
-            | Eat -- Consume an entity (dead or living!)
-            | Go Cardinal -- Actor goes to a different location
-            | Take | Put -- Move stuff between world and inventory
-            | Open | Close -- Door, chest, portal...?
-            | Ask | Tell -- Includes reading and writing
-            | Copulate -- Laying eggs???
+            | Eat !EID -- Consume an entity (dead or living!)
+            | Go !Cardinal -- Actor goes to a different location
+
+            | Pickup !EID -- Move object from world to inventory
+            | Drop !EID -- Put inventory object in world
+            | Open !EID -- Open door/container
+            | Close !EID -- Close door/container
+
+            | Ask -- Includes reading
+            | Tell -- Includes writing
             | Rest -- Remain still and recuperate
             | DoNothing -- Really do nothing
-            | Look -- NOTE: Looking around doesn't consume a turn!
+
+            -- NOTE: Looking around doesn't consume a turn!
+            | Look -- Examine world
+            | Inspect !EID -- Examine an entity
   deriving (Eq, Show)
